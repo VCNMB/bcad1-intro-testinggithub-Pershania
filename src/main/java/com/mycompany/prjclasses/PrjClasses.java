889e882caa557bb1fae6d85af08c6a10ed749b67
Pershania <st10481293@vcconnect.edu.za>
2025-03-11 10:40:44 +0200

    added class
    added another class

--- src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,10 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      newClass nc = new newClass(); // creating an object of newClass
+        
+       nc.helloClass(); // have to usevariable name nc to communicate wit the class
+       
+       System.out.println(nc.strSecondClassVar); // output variable from 2nd class
     }
 }
